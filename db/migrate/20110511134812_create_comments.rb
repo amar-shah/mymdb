d9dc279023a701e82b87886d7fb55c9821a5c8f2
Amar Shah <amar.shah@xbridge.com>
2011-05-11 15:04:22 +0100

    Migration corrected

--- db/migrate/20110511134812_create_comments.rb
+++ db/migrate/20110511134812_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.string text
-      t.integer film_id
-      t.integer user_id
+      t.string :text
+      t.integer :film_id
+      t.integer :user_id
       t.timestamps
     end
   end
