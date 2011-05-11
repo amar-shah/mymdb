6cefe0078ad0cf01b3d574c6a0d20c0f9de1ffc7
Amar Shah <amar.shah@xbridge.com>
2011-05-11 16:04:08 +0100

    Adding comments logic

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+  
+  def create
+    unless request.xhr?
+      render :nothing => true
+      return
+    end
+    film = Film.find(params[:film_id])
+    comment = film.comments.create(:text=>params[:add_comment_text],:user=> current_user)
+    
+    if comment.valid?
+      render :partial =>'comments/comments', :locals=>{:comment =>comment} # local return variables to partial
+    else
+      response.status = 422
+      render :nothing =>true
+    end
+  end
+  
+  def destroy
+    Comment.destroy(params[:id])
+    redirect_to film_path(params[:film_id])
+  end
+  
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :film, :user
+  belongs_to :film
+  belongs_to :user
   validates_presence_of :user, :film, :text
 end
